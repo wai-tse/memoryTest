c0b9ab0287786b056d6db6b4a14dd6d978ef11ad
wai.tse <wai.tse.js@gmail.com>
2018-05-16 09:34:33 -0700

    Revised server.js

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const PORT = normalizePort(process.env.PORT || 5000);
 
 const app = express();
 
-const dev = app.get("env") === "production";
-console.log(app.get("env"));
-
 app.disable("x-powered-by");
 app.use(compression());
 app.use(morgan("common"));
